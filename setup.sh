cf4170f5c754357962455b7f91e43c64a6d3848b
Tony DiPasquale <tony@thoughtbot.com>
2013-12-06 12:49:01 -0500

    Fix bugs with permissions

--- setup.sh
+++ setup.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
 # install necessary packages
-echo "Installing Linux packages..."
+echo "\nInstalling Linux packages...\n"
 sudo apt-get install libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev git
 
 # download bluez library
-echo "Downloading Bluez Library..."
+echo "\nDownloading Bluez Library...\n"
 wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.11.tar.xz
 tar -xJf bluez-5.11.tar.xz
 rm bluez-5.11.tar.xz
 cd bluez-5.11
 
 # install bluez library
-echo "Installing Bluez (this could take a while)..."
+echo "\nInstalling Bluez (this could take a while)...\n"
 ./configure --disable-systemd
 make
 sudo make install
 cd ..
 
 # get pibeacon scripts
-echo "Installng PiBeacon..."
+echo "\nInstallng PiBeacon...\n"
 git clone https://github.com/tonyd256/pibeacon-scripts.git
 cd pibeacon-scripts
-cp run_beacon.sh /etc/network/if-up.d/
-ruby beacon.rb
+sudo cp run_beacon.sh /etc/network/if-up.d/
+ruby pibeacon.rb
 cd ..
 echo "Done!"
 

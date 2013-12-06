c861eb77058519efc1c7f14502c33f586fee4475
Tony DiPasquale <tony@thoughtbot.com>
2013-12-06 11:19:31 -0500

    Add setup files

---
+++ setup.sh
@@ -0,0 +1,24 @@
+
+# install necessary packages
+echo "Installing Linux packages..."
+sudo apt-get install libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev git
+
+# download bluez library
+echo "Downloading Bluez Library..."
+wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.11.tar.xz
+tar -xJf bluez-5.11.tar.xz
+rm bluez-5.11.tar.xz
+cd bluez-5.11
+
+# install bluez library
+echo "Installing Bluez (this could take a while)..."
+./configure --disable-systemd
+make
+sudo make install
+cd ..
+
+# get pibeacon script
+git clone https://github.com/tonyd256/pibeacon-scripts.git
+cd pibeacon-scripts
+cp run_beacon.sh /etc/network/if-up.d/
+
